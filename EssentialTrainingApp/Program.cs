cd509c839c951d176e8493cc13708ddd43ea202b
Kyle Melton <KyleMeltonCodes@gmail.com>
2021-08-31 16:39:02 -0500

    moves ETrainingApp and Tests into git repo

---
+++ EssentialTrainingApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EssentialTrainingApp
+{
+    class Program
+    {
+        public static List<string> Words;
+        static void Main(string[] args)
+        {
+            Words = new List<string>();
+            Words.Add("bread");
+            Words.Add("milk");
+            Words.Add("cheese");
+
+            CrazyMathProblem();
+            ReadTextFile();
+            Console.ReadLine();
+        }
+
+        private static void ReadTextFile()
+        {
+            try
+            {
+                using(var sr = new StreamReader(@"C:\temp\test.txt"))
+                {
+                    string contents = sr.ReadToEnd();
+                    Console.WriteLine(contents);
+                }
+            }
+            catch(System.IO.DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Directory not found");
+            }
+        }
+        private static int CrazyMathProblem()
+        {
+            var income = 1000;
+            for(var i = 10; i > 0; i--)
+            {
+                income = income = (income / i);
+            }
+            return income;
+        }
+    }
+}
---
+++ EssentialTrainingTests/AwesomeSauceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EssentialTraining;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class AwesomeSauceTest
+    {
+        [TestMethod]
+        public void IsSauceAwesomeTest()
+        {
+            var testInstance = new AwesomeSauce();
+            testInstance.Sauces.Add("tobasco");
+            testInstance.Sauces.Add("tobasco");
+            testInstance.Sauces.Add("tobasco");
+
+            //expect true
+            Assert.IsTrue(testInstance.IsSauceAwesome("Trailer Trash"));
+
+            //expected false
+            Assert.IsFalse(testInstance.IsSauceAwesome("Ketchup"));
+        }
+    }
+}
